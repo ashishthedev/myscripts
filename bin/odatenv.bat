99558e8e41adc294a1dfc6b3ac5e086706f5dae9
Ashish Anand <ashishthedev@gmail.com>
2014-05-26 12:52:29 +0530

    Google Drive inplace

--- bin/odatenv.bat
+++ bin/odatenv.bat
@@ -4,13 +4,17 @@ REM Entry point where the whole dev environment is set
 REM Place the following line into the cmd promt shortcut and point to this setenv.bat
 REM D:\Windows\system32\cmd.exe /k E:\Dev\WorkSpace\setenv.bat
 
-set XDATDOCSDIR="%APPDIR%"\ODATDOCS
-set PMTAPPDIR=%APPDIR%\pmtsdat
-set PYTHONPATH=%PYTHONPATH%;%XDATDOCSDIR%\myscripts\PythonModules
+
+
+set XDATDOCSDIR=%APPDIR%\ODATDocs
+set PMTAPPDIR="%APPDIR%"\pmtsdat
 set RELATIVEPATH=Myscripts\bin\alias.txt
 set TCL_LIBRARY=b:\Python27\tcl\tcl8.5\
-doskey /MACROFILE=%XDATDOCSDIR%\%RELATIVEPATH%
-set PYTHONDONTWRITEBYTECODE=1
+set GOPATH=%APPDIR%\goscripts
 
+doskey /MACROFILE="%XDATDOCSDIR%\%RELATIVEPATH%"
+set PYTHONDONTWRITEBYTECODE=1
+set PYTHONPATH=%XDATDOCSDIR%\myscripts\PythonModules\
 
 pushd %XDATDOCSDIR%\myscripts
+
